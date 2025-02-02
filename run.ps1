b4bd58635dc1b7522054e3e75674f82e6012ff0a
MaxHastings <max.jacob.hastings@gmail.com>
2025-02-02 04:56:51 -0600

    update run command

--- run.ps1
+++ run.ps1
@@ -3,4 +3,4 @@ $ErrorActionPreference = "Stop"
 
 # Run the container
 Write-Host "Running Docker container..."
-docker run --gpus all -p 2222:22 -p 8080:8080 -it --rm kolo
+docker run --gpus all -p 2222:22 -p 8080:8080 -it -d --name kolo_container kolo
